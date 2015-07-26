3c2d3ff4bdc7bfe116dfd02152dadd26f92f74b5
James Royal <jhr.atx@gmail.com>
2015-07-26 17:57:07 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pyIDS',
     packages=['pyIDS'],
-    version='0.2.1',
+    version='0.2.2',
     description='Python wrapper for the IDS(IBM Bluemix Devops Services) API',
     author='James Royal',
     author_email='jhr.atx@gmail.com',
     url='https://github.com/jroyal/pyIDS',
-    download_url='https://github.com/jroyal/pyIDS/tarball/0.2.1',
+    download_url='https://github.com/jroyal/pyIDS/tarball/0.2.2',
     install_requires=[
         "requests",
         "xmltodict",
